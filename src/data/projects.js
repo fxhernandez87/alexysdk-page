01d2f9955bda3f8cd2dda488bcc961c9ac38cdb1
alexys <alexysbike@gmail.com>
2018-06-04 17:56:49 -0300

    Adding traduction to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,7 @@ export default {
       name: 'Project Sophia',
       description: {
         es: 'Proyecto Open Source, Aplicación para organizar productos, listas de precios y pedidos. Actualmente en desarrollo',
+        en: 'Open Source, helps to organize products, price\'s lists y orders. In development',
       },
       stack: ['react', 'rematch', 'axios', 'bulma', 'node', 'express', 'mongo'],
       links: [
@@ -28,11 +29,12 @@ export default {
       name: 'Notes Live Coding',
       description: {
         es: 'Aplicación de notas con etiquetas similar a Keep de google. Aplicación creada con fines educacionales y como ayuda a quien desee aprender y usar estas tecnologías',
+        en: 'Notes and Labels application similar to Keep app from google. Created with educational purpose',
       },
       stack: ['react', 'redux', 'sagas', 'axios', 'bulma', 'node', 'express', 'swagger', 'mongo'],
       links: [
         {
-          title: 'Repository',
+          title: 'Repo',
           img: 'logo/github.png',
           href: 'https://github.com/DKCoders/notes-live-coding',
         },
@@ -42,6 +44,7 @@ export default {
       name: 'Logemp',
       description: {
         es: 'Aplicación que permite gestionar procesos de envíos entre dos puntos.',
+        en: 'Application that allows to manage shipping process between points.',
       },
       stack: ['react', 'rematch', 'axios', 'bulma'],
       links: [
@@ -56,6 +59,7 @@ export default {
       name: 'VJ Colors: Formulations',
       description: {
         es: 'Aplicación que te ayuda desarrollar, calcular y organizar formulas químicas. Desarrollada con Codeigniter, AngularJS y Bootstrap.',
+        en: 'Application that helps develop, calculate and organize chemical formulas.',
       },
       stack: ['angularjs', 'bootstrap', 'codeigniter', 'mysql'],
     },
@@ -63,6 +67,7 @@ export default {
       name: 'Battle Simulator',
       description: {
         es: 'En esta aplicación sencilla puedes probar el sistema de batalla TinWill, realizando simulaciones de batallas y ver los resultados. Se puede usar para diseñar las unidades ajustando los Stats de los actores de la simulación.',
+        en: 'This simple application is used to test the battle system of TinWill with simulations in real-time of battles. Used to design and adjust the stats of battle units.',
       },
       stack: ['angularjs', 'angularmaterial', 'node', 'express'],
     },
@@ -70,13 +75,15 @@ export default {
       name: 'Soluciones Gerenciales',
       description: {
         es: 'Aplicación web usada por la Empresa Soluciones Gerenciales para gestionar los cursos que se dictán. Originalmente desarrolada en Codeignitar y luego migrada a Symphony.',
+        en: 'Web application used by Soluciones Gerenciales to manage the dictated courses. Originally developed in Codeigniter.',
       },
       stack: ['codeigniter', 'bootstrap', 'mysql', 'jquery'],
     },
     {
       name: 'Game Of Life',
       description: {
-        es: 'Mi versión de El Juego de La Vida. Desarrollada principalmente en codigo nativo Javascript.',
+        es: 'Mi versión de El Juego de La Vida. Desarrollada principalmente con codigo nativo Javascript.',
+        en: 'My version of The Game of the Life. Developed mainly with Javascript native code.',
       },
       stack: ['bootstrap', 'js'],
     },
@@ -86,6 +93,7 @@ export default {
       name: 'Lista Pro',
       description: {
         es: 'Aplicación Android Híbrida especial para vendedores o cualquiera que trabaje con lista de precios. La app te permite cargar una lista de precios, realizar busquedas en la misma, generar pedidos y enviarlos vía correo electrónico.',
+        en: 'Android Hybrid Application for sellers or anyone who use large price lists. Made with Ionic and Cordova Phonegap for Android. Sophia project predecesor.',
       },
       stack: ['angularjs', 'ionic', 'sqlite'],
     },
@@ -93,6 +101,7 @@ export default {
       name: 'Torque App',
       description: {
         es: 'Aplicación Android que permité al usuario buscar repuestos automotrices en las tiendas de repuestos registradas dentro de la aplicación, pudiendo filtrar las busquedas ya sea limitando el radio de busqueda o tipo de tienda en particular, y así encontrar la mejor opción de compra. La app también ayuda a concretar la operación de compra, sirviendo de interfaz entre el comprador y la tienda.',
+        en: 'Android Hybrid Application to locate auto spare parts between city’s stores.',
       },
       stack: ['angularjs', 'ionic', 'sqlite', 'node', 'express', 'mongo'],
     },
@@ -102,13 +111,15 @@ export default {
       name: 'Bomber Dodge',
       description: {
         es: 'Juego 2D del tipo scroll. Como dice el nombre, solo esquiva las bombas.',
+        en: 'Scroll platform 2D game.',
       },
       stack: ['unity'],
     },
     {
       name: 'Space Ship Game',
       description: {
-        es: 'Juego Simple 2D de Naves Espaciales. Prototipo. Desarrollado en Phaser.io.',
+        es: 'Juego Simple 2D de Naves Espaciales. Prototipo.',
+        en: 'Simple 2D Spaceship game. Prototype.',
       },
       stack: ['phaser'],
     },
